e5962e6827e6a8f8864d767b18a9244fb8ec6e27
mpochiro <mpochiro7@gmail.com>
2018-01-18 21:23:13 -0700

    final push of code

---
+++ bingbong.js
@@ -0,0 +1,14 @@
+let i = 0;
+
+while (i < 100) {
+  i++;
+
+if ((i % 15 )===0) {document.write("BingBong")}
+else{
+if ((i % 3 )===0) {document.write("Bing")}
+else{ if((i % 5 )===0) {document.write("Bong")}
+else{document.write(i)};
+}}
+
+
+}
